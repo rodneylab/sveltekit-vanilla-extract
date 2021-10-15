d3ba01721ad550765b0507706b291f02ad34539e
Rodney Lab <codesign@rodneylab.com>
2021-10-15 15:47:26 +0100

    fix: 💫 update config to allow vanilla-extaract build

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { imagetools } from 'vite-imagetools';
@@ -9,13 +10,17 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
 			define: {
 				'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString())
 			},
-			plugins: [vanillaExtractPlugin(), imagetools({ force: true })]
+			plugins: [vanillaExtractPlugin(), imagetools({ force: true })],
+			ssr: {
+				noExternal: ['@vanilla-extract/css', '@vanilla-extract/css/fileScope']
+			}
 		}
 	}
 };
